e3c3dafaab7951116114d3cee66f9332d525642a
Ceiara Madrigal <90419840+MadCodes9@users.noreply.github.com>
2022-05-27 15:53:41 -0700

    Updated formatting

--- TwoSum.py
+++ TwoSum.py
@@ -5,12 +5,12 @@ from typing import List
 def twoSum(arg1: List[int], arg2):
    # TODO: Sort the numbers before function call to iterate values quicker
    sort = sorted(arg1)
-   print(list(sort))
 
    # TODO: Use takewhile to return a shorten array of values that are less 
    # than the target 
    shortarr = list(itertools.takewhile(lambda x: x < arg2, sort))
-   print(shortarr) 
+   str1 = "Shorten Array: {0}".format(shortarr)
+   print(str1) 
 
    #TODO: Find two values in the shorten array that are equal to the target
    i = 0
@@ -20,8 +20,8 @@ def twoSum(arg1: List[int], arg2):
    while i < length:
        sum = shortarr[i] + shortarr[j]
        if sum == arg2:
-          str1 = "[{0}, {1}]".format(i,j)
-          print(str1)
+          str2 = "Target {0} found at index [{1}, {2}]".format(arg2, i,j)
+          print(str2)
           break
        if i >= j:
           i+=1
@@ -31,6 +31,8 @@ def twoSum(arg1: List[int], arg2):
 def main():
     nums = [3, 5, 10, 1, 6]
     target = 9
+    str = "Original Array: {0}".format(list(nums))
+    print(str)
     twoSum(nums, target)
 
 if __name__ == "__main__":
